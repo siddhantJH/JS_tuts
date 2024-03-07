083c5f131e35e02d2c638c3234b197cfeacc6481
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-03-07 14:27:27 +0000

    basic add to list

---
+++ 05_dom/revise.js
@@ -0,0 +1,27 @@
+const bg=document.querySelector(".root")
+const liList=document.querySelectorAll("li")
+bg.style.backgroundColor="black"
+liList.forEach((h)=>[
+    h.style.color="white"
+])
+
+function addNew(element){
+const newText=document.createTextNode(`${element}`)
+const newli=document.createElement('li')
+newli.setAttribute("style","color:white")
+newli.appendChild(newText)
+document.querySelector('ul').appendChild(newli)
+}
+addNew('Flat')
+addNew('Trip to Europe')
+// addNew('Trip to japan')
+addNew('Get Married')
+
+
+
+function AddElement()
+{
+const element=document.querySelector('.enterItem')
+const t=element.value
+addNew(t)
+}
